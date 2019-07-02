3a8e3629993266fd794595f64c33d02082216e17
meetdhaliwal <50705838+meetdhaliwal@users.noreply.github.com>
2019-07-02 17:46:14 -0400

    Add files via upload

---
+++ mutant/ConsoleApp23/Class1.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace mutant
+{
+    public class Calc
+    {
+        private double firstNumber;
+        private double secondNumber;
+
+
+        public Calc()
+        {
+            firstNumber = 1;
+            secondNumber = 1;
+        }
+
+        public Calc(double firstNumber, double secondNumber)
+        {
+            this.firstNumber = firstNumber;
+            this.secondNumber = secondNumber;
+        }
+
+
+
+        public double Addition()
+        {
+            return Math.Round((firstNumber + secondNumber), 2);
+        }
+
+        public double Subtraction()
+        {
+            return Math.Round((firstNumber - secondNumber), 2);
+        }
+
+        public double Multiplication()
+        {
+            return Math.Round((firstNumber * secondNumber), 2);
+        }
+
+        public double Division()
+        {
+            try
+            {
+                return Math.Round((firstNumber * secondNumber), 2);
+            }
+            catch (DivideByZeroException)
+            {
+                return (0);
+            }
+
+
+        }
+
+    }
+}
+
+
